6e8fb44d07c0dee4cb1c0d691c9ba7ee34f39abe
zampovaj <zampovaj.06@spst.eu>
2025-05-02 16:42:04 +0200

    basic dtos (AnalyzeCodeRequestDto.cs, AnalyzeCodeResponseDto.cs, CodeErrorDto.cs)

---
+++ CodeAssistant/CodeAssistant/API/DTOs/AnalyzeCodeDto.cs
@@ -0,0 +1,21 @@
+﻿namespace CodeAssistant.API.DTOs
+{
+    public class AnalyzeCodeRequestDto
+    {
+        public string Code { get; set; }
+    }
+    public class AnalyzeCodeResponseDto
+    {
+        public string Code { get; set; }
+
+    }
+
+    public class CodeErrorDto
+    {
+        public int Line { get; set; }
+        public string Message { get; set; }
+        public string Code { get; set; }
+        public string Type { get; set; } // warning, error,...
+    }
+
+}
