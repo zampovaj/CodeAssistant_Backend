6b2ede6d4ad5970f49e4a3e19e03aa321ef5d566
zampovaj <zampovaj.06@spst.eu>
2025-05-02 21:34:32 +0200

    ICodeAnalyzer.cs interface

---
+++ CodeAssistant/CodeAssistant/Domain/Interfaces/ICodeAnalyzer.cs
@@ -0,0 +1,14 @@
+﻿using CodeAssistant.Domain.Models;
+
+namespace CodeAssistant.Domain.Interfaces
+{
+    public interface ICodeAnalyzer
+    {
+        /// <summary>
+        /// Detects errors in code nd returns them as a collection
+        /// </summary>
+        /// <param name="codeSnippet">Code snippet to be analyzed</param>
+        /// <returns>A collection of <see cref="CodeError"/> errors</returns>
+        IReadOnlyCollection<CodeError> Analyze(CodeSnippet codeSnippet);
+    }
+}
