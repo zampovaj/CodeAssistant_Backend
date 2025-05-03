555f6ba1d84eb0cdd78c015633f54a68f85a5dc0
zampovaj <zampovaj.06@spst.eu>
2025-05-03 14:59:51 +0200

    AnalyzeCodeUseCase documentation

--- CodeAssistant/CodeAssistant/Application/UseCases/AnalyzeCodeUseCase.cs
+++ CodeAssistant/CodeAssistant/Application/UseCases/AnalyzeCodeUseCase.cs
@@ -1,17 +1,34 @@
 ﻿using CodeAssistant.Domain.Interfaces;
 using CodeAssistant.Domain.Models;
+using System;
 
 namespace CodeAssistant.Application.UseCases
 {
+    /// <summary>
+    /// Use case for analyzing a code snippet and returning a collection of detected code errors.
+    /// </summary>
     public class AnalyzeCodeUseCase
     {
         private readonly ICodeAnalyzer _codeAnalyzer;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AnalyzeCodeUseCase"/> class.
+        /// </summary>
+        /// <param name="codeAnalyzer">
+        /// The code analyzer implementation used to process code and detect errors.
+        /// </param>
         public AnalyzeCodeUseCase(ICodeAnalyzer codeAnalyzer)
         {
             _codeAnalyzer = codeAnalyzer;
         }
 
-        public IReadOnlyCollection<CodeError> Execute (CodeSnippet snippet)
+        /// <summary>
+        /// Executes the code analysis on the provided code snippet.
+        /// </summary>
+        /// <param name="snippet">The <see cref="CodeSnippet"/> to analyze.</param>
+        /// <returns>
+        /// A read-only collection of <see cref="CodeError"/> representing the issues found in the code.
+        /// </returns>
+        public IReadOnlyCollection<CodeError> Execute(CodeSnippet snippet)
         {
             return _codeAnalyzer.Analyze(snippet);
         }
