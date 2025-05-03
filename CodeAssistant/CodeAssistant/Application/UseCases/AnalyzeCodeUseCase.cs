dcd81fc535c423b3bf238a7a95e9b93a8e586deb
zampovaj <zampovaj.06@spst.eu>
2025-05-03 14:56:10 +0200

    AnalyzeCodeUseCase

---
+++ CodeAssistant/CodeAssistant/Application/UseCases/AnalyzeCodeUseCase.cs
@@ -0,0 +1,19 @@
+﻿using CodeAssistant.Domain.Interfaces;
+using CodeAssistant.Domain.Models;
+
+namespace CodeAssistant.Application.UseCases
+{
+    public class AnalyzeCodeUseCase
+    {
+        private readonly ICodeAnalyzer _codeAnalyzer;
+        public AnalyzeCodeUseCase(ICodeAnalyzer codeAnalyzer)
+        {
+            _codeAnalyzer = codeAnalyzer;
+        }
+
+        public IReadOnlyCollection<CodeError> Execute (CodeSnippet snippet)
+        {
+            return _codeAnalyzer.Analyze(snippet);
+        }
+    }
+}
