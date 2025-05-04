22f0ce144b0a85c305515863d606d5a87707b79b
zampovaj <zampovaj.06@spst.eu>
2025-05-04 18:23:45 +0200

    Program.cs setup

--- CodeAssistant/CodeAssistant/Program.cs
+++ CodeAssistant/CodeAssistant/Program.cs
@@ -1,13 +1,28 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
+// Swagger/OpenAPI config
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Use CORS middleware
+app.UseCors("AllowAll");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
