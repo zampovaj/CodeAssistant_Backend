434e23c076e1ccebafa57fd41f6e064874eef1a6
zampovaj <zampovaj.06@spst.eu>
2025-05-04 18:28:31 +0200

    MockCodeAnalyzer.cs

---
+++ CodeAssistant/CodeAssistant/Infrastructure/Mock/CodeAnalyzer.cs
@@ -0,0 +1,13 @@
+﻿using CodeAssistant.Domain.Interfaces;
+using CodeAssistant.Domain.Models;
+
+namespace CodeAssistant.Infrastructure.Mock
+{
+    public class MockCodeAnalyzer : ICodeAnalyzer
+    {
+        public IReadOnlyCollection<CodeError> Analyze(CodeSnippet codeSnippet)
+        {
+            return new List<CodeError>() { new CodeError(1, "message", "A205", ErrorType.Warning) };
+        }
+    }
+}
