7fd82dddf9dd0ae00ae8f26dc154dc34b2e66b08
zampovaj <zampovaj.06@spst.eu>
2025-05-04 18:34:57 +0200

    DependencyInjection.cs

---
+++ CodeAssistant/CodeAssistant/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using CodeAssistant.Domain.Interfaces;
+using CodeAssistant.Infrastructure.Mock;
+
+namespace CodeAssistant.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddScoped<ICodeAnalyzer, MockCodeAnalyzer>();
+            return services;
+        }
+    }
+}
