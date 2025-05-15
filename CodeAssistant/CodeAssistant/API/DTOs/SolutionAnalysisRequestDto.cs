2eea068d672fdf588e18e4262525ea89340d7baf
zampovaj <zampovaj.06@spst.eu>
2025-05-15 18:12:06 +0200

    Solution model and SolutionAnalysisRequestDto

---
+++ CodeAssistant/CodeAssistant/API/DTOs/SolutionAnalysisRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeAssistant.API.DTOs
+{
+    public class SolutionAnalysisRequestDto
+    {
+        public IFormFile ZipFile { get; set; }
+        public SolutionAnalysisRequestDto(IFormFile zipFile)
+        {
+            ZipFile = zipFile;
+        }
+    }
+}
---
+++ CodeAssistant/CodeAssistant/Domain/Models/Solution.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeAssistant.Domain.Models
+{
+    public class Solution
+    {
+        public string Path { get; }
+        public Solution(string path)
+        {
+            Path = path;
+        }
+    }
+}
