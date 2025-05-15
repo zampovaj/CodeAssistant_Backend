04fd9c9814493696c4e304340a09db3239b8a96f
zampovaj <zampovaj.06@spst.eu>
2025-05-15 18:22:41 +0200

    doczmentation

--- CodeAssistant/CodeAssistant/API/DTOs/AnalyzeCodeResponseDto.cs
+++ CodeAssistant/CodeAssistant/API/DTOs/AnalyzeCodeResponseDto.cs
@@ -21,27 +21,6 @@
         Error
     }
 
-    /// <summary>
-    /// Data Transfer Object representing a code analysis request.
-    /// </summary>
-    public class AnalyzeCodeRequestDto
-    {
-        private const int maxCodeLength = 10_000;
-
-        /// <summary>
-        /// The source code to be analyzed.
-        /// </summary>
-        public string Code { get; }
-
-        /// <summary>
-        /// Initializes a new instance of <see cref="AnalyzeCodeRequestDto"/>.
-        /// </summary>
-        /// <param name="code">The code to analyze.</param>
-        public AnalyzeCodeRequestDto(string code)
-        {
-            Code = code;
-        }
-    }
 
     /// <summary>
     /// Data Transfer Object representing the result of a code analysis.
