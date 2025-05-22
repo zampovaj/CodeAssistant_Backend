d941dfaddaad9345eb462c746126b20dfd5fd5c1
zampovaj <zampovaj.06@spst.eu>
2025-05-22 16:28:08 +0200

    Application/interfaces + corresponding classes from Infrastructure - documentation

--- CodeAssistant/CodeAssistant/Application/Interfaces/ICodeCompilationBuilderService.cs
+++ CodeAssistant/CodeAssistant/Application/Interfaces/ICodeCompilationBuilderService.cs
@@ -3,8 +3,14 @@ using Microsoft.CodeAnalysis;
 
 namespace CodeAssistant.Application.Interfaces
 {
+    /// <summary>
+    /// Service used to turn code into Compilation, to prepare it for further analysis
+    /// </summary>
     public interface ICodeCompilationBuilderService
     {
+        /// <summary>
+        /// Accepts <see cref="CodeSnippet"/> and converts it to <see cref="Compilation"/>
+        /// </summary>
         public Task<Compilation> CreateCompilationAsync(CodeSnippet codeSnippet);
     }
 }
