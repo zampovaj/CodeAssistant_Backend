6a54dd3868c46ff507c35f68f5472bec90e910ae
zampovaj <zampovaj.06@spst.eu>
2025-05-22 16:32:43 +0200

    usecases/ docs

--- CodeAssistant/CodeAssistant/Application/UseCases/AnalyzeSolutionUseCase.cs
+++ CodeAssistant/CodeAssistant/Application/UseCases/AnalyzeSolutionUseCase.cs
@@ -13,11 +13,25 @@ namespace CodeAssistant.Application.UseCases
         private readonly ISolutionAnalyzer _solutionAnalyzer;
         private readonly ISolutionBuilderService _solutionBuilderService;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AnalyzeSolutionUseCase"/> class.
+        /// </summary>
+        /// <param name="solutionAnalyzer">
+        /// The solution analyzer implementation used to process code and detect errors.
+        /// </param>
         public AnalyzeSolutionUseCase(ISolutionAnalyzer solutionAnalyzer, ISolutionBuilderService solutionBuilderService)
         {
             _solutionAnalyzer = solutionAnalyzer;
             _solutionBuilderService = solutionBuilderService;
         }
+
+        /// <summary>
+        /// Asnychronous mehtod, that executes the solution analysis on the provided solution.
+        /// </summary>
+        /// <param name="solutionReference">The <see cref="SolutionReference"/> to analyze.</param>
+        /// <returns>
+        /// An <see cref="AnalyzedSolution"/> containing collection of projects with collections of code errors.
+        /// </returns>
         public async Task<AnalyzedSolution> ExecuteAsync(SolutionReference solutionReference)
         {
             Solution solution = await _solutionBuilderService.CompileAsync(solutionReference);
