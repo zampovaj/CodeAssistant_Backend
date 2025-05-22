0c1bfa6106a57ccd71bdf5f0e7d2dd2df1eec61c
zampovaj <zampovaj.06@spst.eu>
2025-05-22 18:28:54 +0200

    domain/interfaces - docs

--- CodeAssistant/CodeAssistant/Domain/Interfaces/ICodeAnalyzer.cs
+++ CodeAssistant/CodeAssistant/Domain/Interfaces/ICodeAnalyzer.cs
@@ -3,10 +3,13 @@ using Microsoft.CodeAnalysis;
 
 namespace CodeAssistant.Domain.Interfaces
 {
+    /// <summary>
+    /// Analyzes code for errors.
+    /// </summary>
     public interface ICodeAnalyzer
     {
         /// <summary>
-        /// Detects errors in code nd returns them as a collection
+        /// Asnychronous method which detects errors in code nd returns them as a collection
         /// </summary>
         /// <param name="codeSnippet">Code snippet to be analyzed</param>
         /// <returns>A collection of <see cref="CodeError"/> errors</returns>
--- CodeAssistant/CodeAssistant/Domain/Interfaces/ISolutionAnalyzer.cs
+++ CodeAssistant/CodeAssistant/Domain/Interfaces/ISolutionAnalyzer.cs
@@ -2,8 +2,16 @@
 
 namespace CodeAssistant.Domain.Interfaces
 {
+    /// <summary>
+    /// Analyzed solution for errors.
+    /// </summary>
     public interface ISolutionAnalyzer
     {
+        /// <summary>
+        /// Asnychronous method which detects errors in solution and returns them as part of <see cref="AnalyzedSolution"/> model
+        /// </summary>
+        /// <param name="solution">Solution to be analyzed</param>
+        /// <returns><see cref="AnalyzedSolution"/> containing analyzed projects with collections of errors.</returns>
         public Task<AnalyzedSolution> AnalyzeSolutionAsync(Solution solution);
     }
 }
